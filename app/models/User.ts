1146196230593ee0a93e7ac4d8a4dae26a415372
Cameron Young <cameronjyoung123@gmail.com>
2021-05-06 11:51:55 +0100

    added interface for user and updated password to be type text

--- app/models/User.ts
+++ app/models/User.ts
@@ -1,5 +1,14 @@
 import { DataTypes, Model } from '../../deps.ts';
 
+export interface UserInterface {
+	id?: string;
+	username: string;
+	password: string;
+	firstname: string;
+	lastname: string;
+	role: string;
+}
+
 export class User extends Model {
 	static table = 'users';
 	static timestamps = true;
@@ -7,7 +16,7 @@ export class User extends Model {
 	static fields = {
 		id: { primaryKey: true, autoIncrement: true },
 		username: {type: DataTypes.STRING},
-		password: {type: DataTypes.STRING},
+		password: {type: DataTypes.TEXT},
 		firstname: {type: DataTypes.STRING},
 		lastname: {type: DataTypes.STRING},
 		role: {type: DataTypes.STRING},
